8a71f244c04be7c07276954ef201b2916a50e6b5
suzelcarvalho <suzelbrito@gmail.com>
2021-10-26 18:00:11 +0000

    atualização

--- src/main/java/application/controllers/LivroController.java
+++ src/main/java/application/controllers/LivroController.java
@@ -2,9 +2,10 @@ package application.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import application.models.Livro;
 import application.repositories.LivroRepository;
-
 import org.springframework.ui.Model;
 
 @Controller
@@ -16,13 +17,21 @@ public class LivroController {
     public String list(Model model) {
         model.addAttribute("livros", livrosRepo.findAll());
         return "list.jsp"; 
+        
     }
-  public String listar(){
+    public String listar(){
     return "livro/list.jsp";
   }
   @RequestMapping("/insert")
   public String formInsert(){
     return "insert.jsp";
   } 
+ @RequestMapping(value="/insert",method=RequestMethod.POST)
+  public String saveInsert(@RequestParam("titulo")String titulo){
 
+    Livro livro=new Livro();
+    livro.setTitulo(titulo);
+    livrosRepo.save(livro);
+    return "redirect:/livro/list";
+  } 
 }
